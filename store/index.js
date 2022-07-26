6641f1c719a000b7db74c383918ccba549d00676
luqmanulhakim666 <luqmanulh25@gmail.com>
2022-07-27 02:59:34 +0700

    create vuex for saving data

---
+++ store/index.js
@@ -0,0 +1,23 @@
+export const state = () => ({
+  applications: [],
+});
+
+export const mutations = {
+  ADD_ITEM(state, payload) {
+    state.applications.push(payload);
+  },
+  REFRESH_ITEM(state) {
+    state.applications = [];
+  },
+};
+
+export const actions = {
+  onCreate({ commit }, item) {
+    let parseItem = JSON.parse(JSON.stringify(item));
+    commit("ADD_ITEM", parseItem);
+  },
+
+  onRefresh({ commit }) {
+    commit("REFRESH_ITEM");
+  },
+};
